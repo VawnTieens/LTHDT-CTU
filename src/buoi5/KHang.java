07f7fc09ea99a07d4ac9a21cce556132a69402d8
Trương Văn Tiến <takeiteasy.txt@gmail.com>
2024-12-07 14:45:42 +0700

    Update KHang.java

--- src/buoi5/KHang.java
+++ src/buoi5/KHang.java
@@ -4,19 +4,13 @@ import java.util.Scanner;
 
 public class KHang {
 	private String ten;
-	private boolean gtinh; //true M, false FM
+	private boolean gtinh;
 	private int nsinh;
 	
-	//cac phuong thuc xay dung
 	public KHang() {
-		ten = "Nguyen Van An";
+		ten = "Nguyen Van A";
 		gtinh = true;
-		nsinh = 1980;
-	}
-	public KHang (String t, boolean gt, int ns) {
-		ten = t;
-		gtinh = gt;
-		nsinh = ns;
+		nsinh = 2000;
 	}
 	public KHang(KHang k) {
 		ten = k.ten;
@@ -25,14 +19,16 @@ public class KHang {
 	}
 	public void nhap() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Nhap ho ten: ");
+		System.out.println("Nhap ten khach hang: ");
 		ten = sc.nextLine();
-		System.out.println("Nhap gioi tinh (true-Nam, false-Nu): ");
+		System.out.println("Nhap gioi tinh(True-Nam, False-Nu): ");
 		gtinh = sc.nextBoolean();
 		System.out.println("Nhap nam sinh: ");
 		nsinh = sc.nextInt();
 	}
 	public void in() {
-		System.out.println(ten +"/"+gtinh+"/"+nsinh);
+		System.out.println("Ten khach hang: " + ten);
+		System.out.println("Gioi tinh: " + gtinh);
+		System.out.println("Nam sinh: " + nsinh);
 	}
 }
